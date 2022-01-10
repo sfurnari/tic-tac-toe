90d619939d871f3da4abb409018ab1f286501795
Steven Furnari <s.furnari@hotmail.com>
2022-01-10 20:41:12 +1100

    win condition finished

--- js/main.js
+++ js/main.js
@@ -1,25 +1,69 @@
 console.log('js loaded')
 
-const player1 = 'Player 1 ( x )'
-const player2 = 'Player 2 ( o )'
+let currentGame = ["", "", "", "", "", "", "", "", ""]
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
+const player1 = 'X';
+const player2 = 'O';
 let currentPlayer = player1;
 
+let xWins = 0;
+let oWins = 0;
+
+
+const winCheck = function(){
+    let gameWon = false;
+    for (let i = 0; i < winConditions.length; i++) { // loops through array of win conditions
+
+        const isWin = winConditions[i]; // ---
+        let a = currentGame[isWin[0]];  //    |    assigns variable to each index of win condition [0, 1, 2]
+        let b = currentGame[isWin[1]];  //    |--- to match with current game status.
+        let c = currentGame[isWin[2]];  // ___|
+
+        if (a === "" || b === "" || c === "") { // if any game squares haven't been clicked, continue through the loop
+            continue;
+        }        
+        if (a === b && b === c){ // matched win condition in array of current game
+            gameWon = true;
+        }
+    }
+    if (gameWon) {
+        $('.game-status').text(`${currentPlayer} wins!!!`)
+    }
+};
+
 const playerChange = function(){
-    currentPlayer = currentPlayer === player1 ? player2 : player1
-}
+    if(currentPlayer === player1){
+        currentPlayer = player2;
+    } else {
+        currentPlayer = player1;
+    }
+};
 
 $('.square').on('click', function(e){
      const id = e.target.id;
+
  //_________Is square already clicked?__________   
     if($('#' + id).text() != ""){
         return;
     }
 // _________Player Input____________
     if (currentPlayer === player1){
-        $('#' + id).text('x')
+        $('#' + id).text('x');
     } else {
         $('#' + id).text('o')
     }
+    currentGame[Number(id)] = currentPlayer
+    winCheck();
     playerChange();
-})
+    console.log(currentGame)
+});
