a566c65f4e7a8c9920bf1c142f3ff4bf1613d36a
Steven Furnari <s.furnari@hotmail.com>
2022-01-13 16:04:15 +1100

    styling and extra fixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 console.log('js loaded')
 
+
 let liveGame = true;
 let cpuGame = false;
 let currentGame = ['', '', '', '', '', '', '', '', '']
@@ -20,6 +21,7 @@ const player2 = 'o';
 let currentPlayer = player1;
 let player1Wins = 0;
 let player2Wins = 0;
+let drawCounter = 0
 
 
 const cpuChooses = function(){
@@ -48,11 +50,6 @@ const playerChange = function(){
 const winCheck = function(){
     let gameWon = false;
     let $gameResultMessage = $('#game-result')
-    const openModal = function (){
-        $('.modal').css('display', 'block');
-        $('.modal-content').css('display', 'block');
-    }
-    
 
     for (let i = 0; i < winConditions.length; i++) { // loops through array of win conditions
         const isWin = winConditions[i];         // ---
@@ -74,20 +71,20 @@ const winCheck = function(){
         if (currentPlayer === player1){
             player1Wins++
             $('#player1-wins').text(`[X] wins: ${player1Wins}`)
-            openModal()
+            openWinModal()
         } else {
             player2Wins++
             $('#player2-wins').text(`[O] wins: ${player2Wins}`)
-            openModal()
+            openWinModal()
         }
     } 
-
     if (!gameWon && !currentGame.includes('')){ // if game results in a draw
         liveGame = false
-        $gameResultMessage.text("Game is a draw")
-        openModal()
+        drawCounter++
+        $gameResultMessage.text("Draw!")
+        $('#draw').text(`Draws: ${drawCounter}`)
+        openWinModal()
     };
-
 }; // winCheck() -  checks for win or draw
 
 const newGame = function(){
@@ -98,21 +95,25 @@ const newGame = function(){
 }; // newGame() - sets game to turn 1
 
 const resetGame = function(){
-    $('#player1-wins').text('[X] wins: 0')
-    $('#player2-wins').text('[O] wins: 0')
+    $('#player1-wins').text('[X] wins: 0');
+    $('#player2-wins').text('[O] wins: 0');
+    $('#draw').text('Draws: 0');
+
     player1Wins = 0;
     player2Wins = 0;
+    drawCounter = 0;
     newGame();
 }; // resetGame() - resets game & score
 
 const closeModal = function(){
     $('.modal').css('display', 'none');
-    $('.modal-content').css('display', 'none');
+    $('#game-result').css('display', 'block')
 }; // closeModal() - closes modal pop up
 
 const storeLocalData = function(){
     localStorage.setItem('player1wins', `${player1Wins}`);
     localStorage.setItem('player2wins', `${player2Wins}`);
+    localStorage.setItem('drawCounter', `${drawCounter}`);
     localStorage.setItem('currentGame', JSON.stringify(currentGame));
     localStorage.setItem('liveGame', `${liveGame}`)
     localStorage.setItem('cpuGame', `${cpuGame}`)
@@ -121,10 +122,13 @@ const storeLocalData = function(){
 
 const restoreLocalData = function(){  
     player1Wins = parseInt(localStorage.getItem('player1wins'));
-    $('#player1-wins').text(`Player 1 [X] wins: ${player1Wins}`);
+    $('#player1-wins').text(`[X] wins: ${player1Wins}`);
     
     player2Wins = parseInt(localStorage.getItem('player2wins'));
-    $('#player2-wins').text(`Player 2 [O] wins: ${player2Wins}`);
+    $('#player2-wins').text(`[O] wins: ${player2Wins}`);
+
+    drawCounter = parseInt(localStorage.getItem('drawCounter'));
+    $('#draw').text(`Draws: ${drawCounter}`);
 
     currentGame = JSON.parse(localStorage.getItem('currentGame'));
 
@@ -135,34 +139,48 @@ const restoreLocalData = function(){
     currentPlayer = localStorage.getItem('currentPlayer');
 
     for (let i = 0; i < currentGame.length; i++) {
-        const boardPos = currentGame[i];
         $(`#${i}`).text(currentGame[i])   
     } 
 }; // restoreLocalData() - restores local game data
 
+const restoreModal = function(){
+    $('#restore-modal').css('display', 'block');
+    $('.modal-content').css('display', 'block');
+};
+
+const openWinModal = function(){
+    $('#win-modal').css('display', 'block');
+    $('.win-modal-content').css('display', 'block');
+};
+
 $('.square').on('click', function(e){
     const id = e.target.id;
     const idNum = Number(id);
+    let validTurn = true;
     const playGame = function(){
         if (liveGame){ // is game live?
 
-        if ($(`#${id}`).text() != ''){ // has square already been clicked?
-            return;
-        }
+            if ($(`#${id}`).text() != ''){ // has square already been clicked?
+                validTurn = false
+            }
 
-        if (currentPlayer === player1){ // players turn input
-            $(`#${id}`).text('x');          
-        } else {
-            $(`#${id}`).text('o')     
-        }
-        currentGame[idNum] = currentPlayer;
-        winCheck();
-        playerChange();
+            if (currentPlayer === player1){ // players turn input
+                $(`#${id}`).text('x');          
+            } else {
+                $(`#${id}`).text('o')     
+            }
+            currentGame[idNum] = currentPlayer;
+            if (validTurn){
+                winCheck();
+                playerChange();
+            } 
         }
     }
-    if(cpuGame){
+    if (cpuGame){
         playGame();
-        cpuChooses();
+        if (validTurn){
+            cpuChooses();
+        }
 
     } else {
         playGame();
@@ -171,16 +189,14 @@ $('.square').on('click', function(e){
 }); // game is played here
 
 $('#new-game').on('click', function(){
-    newGame();
+    $('#game-result').css('display', 'none')
+    openWinModal();
+
 }); // main menu new game
 
-$('#new-game-yes').on('click', function (){
+$('#no').on('click', function(){
     closeModal();
-    newGame();
-}); // modal yes button
-
-$('#new-game-no').on('click', function(){
-    closeModal();
+    localStorage.clear();
 }); // modal no button
 
 $('#reset-game').on('click', function(){
@@ -189,15 +205,26 @@ $('#reset-game').on('click', function(){
 }); // reset button
 
 $('#play-human').on('click', function(){
-    resetGame();
-    cpuGame = false
+    newGame();
+    cpuGame = false;
+    closeModal();
 }); // play human button
 
 $('#play-computer').on('click', function(){
-    resetGame();
+    newGame();
     cpuGame = true;
+    closeModal();
 });// play computer button
 
 $('#restore').on('click', function(){
     restoreLocalData();
+    closeModal();
 });
+
+$('.close').on('click', function(){
+    closeModal()
+});
+
+if (localStorage.length != 0){
+    $('window').on('load', restoreModal());
+}
